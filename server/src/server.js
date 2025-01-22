c78bdaaa208d75865df1604c4d8aa61a2409b16e
Anjay P.P <anjaypp20@gmail.com>
2025-01-22 19:25:45 +0530

    chore: Set up backend with core dependencies and basic structure

---
+++ server/src/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+})
